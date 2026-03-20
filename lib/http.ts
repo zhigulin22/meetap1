a4daca0d216028143f098307a3b6c563fbab7d60
Artemka <artyom.bagishev@mail.ru>
2026-03-20 05:15:50 +0300

    Delete lib directory

--- lib/http.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function ok<T>(data: T, init?: ResponseInit) {
-  return NextResponse.json(data, init);
-}
-
-export function fail(message: string, status = 400) {
-  return NextResponse.json({ error: message }, { status });
-}
